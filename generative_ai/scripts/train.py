87683dd5d5619e2130a736138b00bbe685302374
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2023-08-25 17:59:07 +0200

    tune

--- generative_ai/scripts/train.py
+++ generative_ai/scripts/train.py
@@ -18,22 +18,26 @@ class CFG:
     n_layer = 8
     n_head = 8
     n_embd = 512
-    dropout = 0.0
-    batch_size = 128
-    lr = 1e-3
-    weight_decay = 0.00
+    dropout = 0.1
+    batch_size = 256
+    lr = 1e-4
+    weight_decay = 0.01
+    save_interval = 1000
 
 
 class GPTDataset(Dataset):
     def __init__(self, tokenizer: Tokenizer, max_length: int) -> None:
         super().__init__()
         self.data = load_dataset("bookcorpus", split="train")  # 1.0B tokens
-        tokenizer.enable_padding(length=max_length + 1)  # add buffer for a single token
+        tokenizer.enable_padding(
+            length=max_length + 1, pad_id=2, pad_token="<|padding|>"
+        )  # add buffer length for a single token
         tokenizer.enable_truncation(max_length=max_length)
         self.tokenizer = tokenizer
 
     def __getitem__(self, index) -> (torch.Tensor, torch.Tensor):
         sentence = self.data[index]["text"]
+        sentence = "<|startoftext|>" + sentence + "<|endoftext|>"
         input_ids = self.tokenizer.encode(sentence).ids
         x = torch.LongTensor(input_ids[:-1])
         y = torch.LongTensor(input_ids[1:])
@@ -45,7 +49,7 @@ class GPTDataset(Dataset):
 
 def main() -> None:
     # system settings
-    device = "cpu"
+    device = "cuda"
     dtype = (
         "bfloat16"
         if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
@@ -86,10 +90,10 @@ def main() -> None:
 
     pbar = tqdm(dataloader)
     for step, (X, Y) in enumerate(pbar):
-        if step % 10 == 0:
-            idx = model.generate(torch.LongTensor([[0]]), max_new_tokens=8)
+        if step % CFG.save_interval == 0:
+            idx = model.generate(torch.LongTensor([[0]]).to(device), max_new_tokens=8)
             sentence = tokenizer.decode(idx.squeeze().detach().cpu().tolist())
-            print(sentence)
+            log_sentence(step, sentence)
 
         X, Y = X.to(device), Y.to(device)
         with cast_ctx:
@@ -99,7 +103,15 @@ def main() -> None:
         scaler.update()
         optimizer.zero_grad()
         pbar.set_postfix_str(f"loss={loss.item():.2E}")
+        log_metrics(step, loss.item())
 
+def log_metrics(step: int, loss: float) -> None:
+    with open(f"{CWD.parent}/artifacts/loss.csv", "a") as f:
+        f.write(f"{step},{loss:.3E}\n")
+
+def log_sentence(step: int, sentence: str) ->None
+    with open(f"{CWD.parent}/artifacts/sentence.txt", "a") as f:
+        f.write(f"[{step}] {sentence}\n")
 
 if __name__ == "__main__":
     main()
